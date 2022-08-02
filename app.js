d4ec6cf60429d04fbc5706604b7ed40fa52286e2
UlisesGabriel97 <ugmendez97@gmail.com>
2022-08-02 12:46:36 -0300

    Subo el TP de Patrones de Diseño MVC terminado

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const app = express();
+const path = require('path');
+const port = 3000;
+
+app.use(express.static(path.resolve(__dirname, "public")))
+
+
+/* Importacion de rutas */
+let homeRouter = require("./routers/home")
+
+/* Rutas */
+app.use("/", homeRouter)
+app.use("/about", homeRouter)
+
+
+app.listen(port, () => { console.log(`Servidor funcionando en http://localhost:${port}`) })
